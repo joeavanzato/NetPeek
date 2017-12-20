387b78712926cced20d8dc77ecc56dacd74b281d
Joe Avanzato <joeavanzato@gmail.com>
2017-12-19 19:59:41 -0500

    Add files via upload

--- TCPScan.py
+++ TCPScan.py
@@ -3,27 +3,61 @@ import argparse
 import threading
 import ftplib
 import random
+import time
+import sys
 
 mailfront = ["Zofia", "Test", "Jack", "Alehandro", "Vladimir", "Boston", "Loquisha", "Mahogany", "Ferrari"]
 mailback = ["gmail.com", "yahoo.com", "hotmail.com"]
 
+global flood_time
 global target_host 
 global target_Ports
-target_ports = []
+target_host = ""
+target_Ports = []
+flood_time = 0
+target_Ports.append("")
 
-
-parser = argparse.ArgumentParser(usage = 'Host (-H) [TARGET-HOST], -ports (-p) [TARGET-PORTS (ex. 21 125 80)], -Anonymous FTP Query (-a), Must use -p with args or -a')
-parser.add_argument("-H", "--Host", help = 'Give Target Host!', default = "", required = True)
-parser.add_argument("-p", "--ports", nargs = '+', help = 'Give Target Port!', default = "")
+parser = argparse.ArgumentParser(usage = 'Host (-H) [TARGET-HOST], Ports for Scanning (-p) [TARGET-PORTS (ex. 21 125 80)], Anonymous FTP Login Query (-a) (No Args), UDP Flooding Enabled (-u 200) where 200 is total msgs sent')
+parser.add_argument("-H", "--Host", help = 'Give Target Host!', required = True)
+parser.add_argument("-p", "--ports", nargs = '+', help = 'Give Target Port!')
 parser.add_argument("-a", "--Anon", action = 'store_true', help = 'Sets Anonymous FTP Query Mode')
+parser.add_argument("-u", "--flood", nargs = 1, help = 'Flood Host with random UDP Datagrams')
 args = parser.parse_args()
-if (str(args.ports) == "") and ((args.Anon) == False): #Must use one of these
+
+if (((target_Ports[0]) == "") and ((args.Anon) == False) and ((args.flood) == "")): #Must use at least one of these
+    print("Missing Parameters!  Must use -u with some ports")
+    print(parser.usage)
+    exit(0)
+elif ((target_Ports[0]) == "") and ((args.flood) == True):
+    print("Must Select Ports for UDP Flooding with -p!")
     print(parser.usage)
     exit(0)
 else:
     target_host = args.Host
     target_Ports = args.ports
+    flood_time = args.flood
 
+def flood_host(host, portlist, flood_time):
+    count = 0
+    for port in portlist:
+        count = count + 1
+        tname = 't-'+str(count)
+        tname = threading.Thread(target = flood_port(host, port, flood_time))
+        tname.start()
+
+def flood_port(host, port, flood_time):
+    current_count = 0
+    temp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    random_data = bytes(random._urandom(512))
+    stop_time = int(time.time()) + flood_time
+    while True:
+        if ((stop_time) < (time.time())) or (current_count > 30): #Artificial Break
+            break
+        else:
+            pass
+        print("Datagram Sent")
+        temp_socket.sendto(random_data, (host, port))
+        current_count = current_count + 1
 
 def query_Login(host):
     try:
@@ -45,7 +79,7 @@ def crawl(connection):
         list = connection.dir()
     except:
         print("Failed Retrieiving Default Directory Contents")
-        return
+        pass
     #for file in list:
     #    name = file.lower()
     #    if ('.php' or '.html' or '.htm' or '.asp') in name:
@@ -56,13 +90,13 @@ def crawl(connection):
 
 def Connect(target_host, target_port):
     try:
-        cursock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        cursock.connect((target_host, target_port))
+        current_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        current_socket.connect((target_host, target_port))
         print("Socket Connected to "+target_host+" on Port "+str(target_port))
-        cursock.send(b'Hello\r\n') #Payload
-        feedback = cursock.recv(100)
+        current_socket.send(b'Hello\r\n') #Payload
+        feedback = current_socket.recv(100)
         print(str(feedback))
-        cursock.close()
+        current_socket.close()
     except socket.error:
         print("Error Connecting on Port "+str(target_port))
 
@@ -83,14 +117,25 @@ def Port_Scan(target_host, target_Ports):
         print(curport)
         x = x + 1
         print("Scanning Port "+str(curport))
-        t1 = threading.Thread(target = Connect(target_host, int(curport)))
-        t1.start()
+        tconnect = threading.Thread(target = Connect(target_host, int(curport)))
+        tconnect.start()
 
 def main():
     print("")
-    if ((target_Ports) != ""):
-        Port_Scan(target_host, target_Ports)
+    if ((flood_time) != 0):
+        tflood = threading.Thread(target = flood_host(target_host, target_Ports, flood_time))
+        tflood.start()
+    else:
+        pass
     if (args.Anon == True):
-        query_Login(target_host)
+        tlogin = threading.Thread(target = query_Login(target_host))
+        tlogin = t3.start()
+    else:
+        pass
+    if ((target_Ports) != ""):
+        tscan = threading.Thread(target = Port_Scan(target_host, target_Ports))
+        tscan.start()
+    else:
+        pass
     print("All Done")
 main()
